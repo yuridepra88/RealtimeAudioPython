a7e49fb7e6db47e85af6509059b2e5927c44c324
yuridepra88 <58724762+yuridepra88@users.noreply.github.com>
2020-04-03 09:59:44 +0200

    Add files via upload

--- Ring/c_diod.pyx
+++ Ring/c_diod.pyx
@@ -15,20 +15,23 @@ cdef extern void compute_c (double* data, double* modulator, int samples, float 
 # static type annotations
 cdef double *cdata
 cdef double *mod_cdata
-cdata = <double *>malloc(2048*cython.sizeof(double))
-mod_cdata = <double *>malloc(2048*cython.sizeof(double))
+
+
+cdata = <double *>malloc(16384*cython.sizeof(double))
+mod_cdata = <double *>malloc(16384*cython.sizeof(double))
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def compute_c1(np.ndarray[double, ndim=1, mode="c"] data not None, np.ndarray[double, ndim=1, mode="c"] mod_data not None, int FRAMES_PER_BUFFER, float T, int over_f, np.ndarray[double, ndim=1, mode="c"] a, np.ndarray[double, ndim=1, mode="c"] b, np.ndarray[double, ndim=1, mode="c"] zi):
+def compute_cython_c(np.ndarray[double, ndim=1, mode="c"] data not None, np.ndarray[double, ndim=1, mode="c"] mod_data not None, int FRAMES_PER_BUFFER, float T, int over_f, np.ndarray[double, ndim=1, mode="c"] a, np.ndarray[double, ndim=1, mode="c"] b, np.ndarray[double, ndim=1, mode="c"] zi):
 
 	cdef unsigned int i
-
+	
 	#resample the two buffers to oversampling rate using 0-padding
 	indices = np.repeat(np.arange(data.shape[0]+2), (over_f-1))[1*(over_f-1):-1*(over_f-1)]      
 	data=np.insert(data, indices, 0)
 	mod_data=np.insert(mod_data, indices, 0)
-	
+
+
 	#move data from python np.arrray to C memory located 
 	for i in xrange(len(data)):
 		cdata[i] = data[i]
@@ -44,7 +47,7 @@ def compute_c1(np.ndarray[double, ndim=1, mode="c"] data not None, np.ndarray[do
 	#low-pass filter applied to computed data
 	new_data, zi= signal.lfilter(b, a, data, zi=zi)
 	
-
+	
 	return new_data[::over_f],zi #take one sample each over_f samples
 
 
